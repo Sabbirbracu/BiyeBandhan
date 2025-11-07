a224224ebe254ab0781fb155cfb56f4b2e59d46e
SabbirAhmad <sabbir.ahmad1@g.bracu.ac.bd>
2025-11-08 00:48:34 +0600

    added the search, completation % features

--- src/app/api/user/other/[id]/route.ts
+++ src/app/api/user/other/[id]/route.ts
@@ -1,38 +1,27 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// GET /api/proxy/user/:id
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+// GET /api/user/other/:id
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   try {
     const token = req.headers.get("authorization");
     const userId = params.id;
-    
 
     if (!userId) {
-      return NextResponse.json(
-        { error: "User ID is required" },
-        { status: 400 }
-      );
+      return NextResponse.json({ error: "User ID is required" }, { status: 400 });
     }
 
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_API}/user/${userId}`,
-      {
-        headers: {
-          Authorization: token || "",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/user/${userId}`, {
+      headers: {
+        Authorization: token || "",
+        "Content-Type": "application/json",
+      },
+    });
 
     const data = await res.json();
     console.log("Fetched user data:", data);
     return NextResponse.json(data, { status: res.status });
   } catch (error) {
     console.error("Proxy fetch error:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch user data" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Failed to fetch user data" }, { status: 500 });
   }
 }
